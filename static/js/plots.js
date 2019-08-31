bf3af7025fdce948421497abbf3206a306efce7d
areed145 <areed145@gmail.com>
2019-08-31 13:28:28 -0500

    uncommented "on-click" for plot change

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,19 +1,19 @@
-// $('#first_cat').on('change', function () {
-//     $.ajax({
-//         url: "/bar",
-//         type: "GET",
-//         contentType: 'application/json;charset=UTF-8',
-//         data: {
-//             'selected': document.getElementById('first_cat').value
+$('#first_cat').on('change', function () {
+    $.ajax({
+        url: "/bar",
+        type: "GET",
+        contentType: 'application/json;charset=UTF-8',
+        data: {
+            'selected': document.getElementById('first_cat').value
 
-//         },
-//         dataType: "json",
-//         success: function (data) {
-//             Plotly.newPlot('bargraph', data);
-//             Plotly.newPlot('bargraph2', data);
-//         }
-//     });
-// })
+        },
+        dataType: "json",
+        success: function (data) {
+            Plotly.newPlot('bargraph', data);
+            Plotly.newPlot('bargraph2', data);
+        }
+    });
+})
 
 setInterval(function () {
     $.ajax({
