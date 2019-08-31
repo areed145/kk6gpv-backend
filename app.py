859062999db8dcab2e3d3e4877c450d6fed572a8
areed145 <areed145@gmail.com>
2019-08-31 11:35:30 -0500

    edits to pages, remove jquery, add pages to app.py

--- app.py
+++ app.py
@@ -44,9 +44,69 @@ def index():
     return render_template('index.html', plot=bar)
 
 
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')
+@app.route('/awc')
+def awc():
+    return render_template('awc.html')
+
+
+@app.route('/wx')
+def wx():
+    return render_template('wx.html')
+
+
+@app.route('/iot')
+def iot():
+    return render_template('iot.html')
+
+
+@app.route('/aprs')
+def aprs():
+    return render_template('aprs.html')
+
+
+@app.route('/aircraft')
+def aircraft():
+    return render_template('aircraft.html')
+
+
+@app.route('/paragliding')
+def paragliding():
+    return render_template('paragliding.html')
+
+
+@app.route('/soaring')
+def soaring():
+    return render_template('soaring.html')
+
+
+@app.route('/n5777v')
+def n5777v():
+    return render_template('n5777v.html')
+
+
+@app.route('/galleries')
+def galleries():
+    return render_template('galleries.html')
+
+
+@app.route('/travel')
+def travel():
+    return render_template('travel.html')
+
+
+@app.route('/scuba')
+def scuba():
+    return render_template('scuba.html')
+
+
+@app.route('/fishing')
+def fishing():
+    return render_template('fishing.html')
+
+
+@app.route('/oilgas')
+def oilgas():
+    return render_template('oilgas.html')
 
 
 @app.route('/test')
