ffbc1fb37885e54c2c9a56ef78dad182db5979c5
areed145 <areed145@gmail.com>
2019-08-31 13:26:11 -0500

    edits to update every few seconds

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,17 +1,32 @@
-$('#first_cat').on('change', function () {
+// $('#first_cat').on('change', function () {
+//     $.ajax({
+//         url: "/bar",
+//         type: "GET",
+//         contentType: 'application/json;charset=UTF-8',
+//         data: {
+//             'selected': document.getElementById('first_cat').value
 
+//         },
+//         dataType: "json",
+//         success: function (data) {
+//             Plotly.newPlot('bargraph', data);
+//             Plotly.newPlot('bargraph2', data);
+//         }
+//     });
+// })
+
+setInterval(function () {
     $.ajax({
         url: "/bar",
         type: "GET",
         contentType: 'application/json;charset=UTF-8',
         data: {
             'selected': document.getElementById('first_cat').value
-
         },
         dataType: "json",
-        success: function (data) {
+    })
+        .done(function (data) {
             Plotly.newPlot('bargraph', data);
             Plotly.newPlot('bargraph2', data);
-        }
-    });
-})
+        })
+}, 1000 * 5);
