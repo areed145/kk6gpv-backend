f1842fbc33af32ace8d941f7f88ce42fee0be687
areed145 <areed145@gmail.com>
2019-09-04 21:32:34 -0500

    clean up, move to helper folder

--- app.py
+++ app.py
@@ -1,52 +1,10 @@
 from flask import Flask, render_template, request
 
-import flickr_api as f
-from figures import figs
-
-import numpy as np
-import pandas as pd
-
-import plotly
-import plotly.graph_objs as go
-import json
-
-from pymongo import MongoClient
+from helpers import figs, flickr
 
 app = Flask(__name__)
 
-f.set_keys(api_key='77a2ae7ea816558f00e4dd32249be54e',
-           api_secret='2267640a7461db21')
-f.set_auth_handler('helpers/auth')
-username = '- Adam Reeder -'
-u = f.Person.findByUserName(username)
-ps = u.getPhotosets()
-
-gals = {}
-for p in ps:
-    pid = p.id
-    title = p.title
-    count_photos = p.count_photos
-    count_views = p.count_views
-    primary = 'https://live.staticflickr.com/8203/'+p.primary+'_'+p.secret+'_q_d.jpg'
-    flickr_link = 'https://www.flickr.com/photos/adamreeder/albums/'+p.id
-    kk6gpv_link = '/galleries/'+p.id
-    photos = {}
-    phs = p.getPhotos()
-    for ph in phs:
-        photos[ph.id] = {
-            'thumb': 'https://live.staticflickr.com/8203/'+ph.id+'_'+ph.secret+'_q_d.jpg',
-            'large': 'https://live.staticflickr.com/8203/'+ph.id+'_'+ph.secret+'_q_h.jpg'
-        }
-    gals[pid] = {
-        'id': pid,
-        'title': title,
-        'count_photos': count_photos,
-        'count_views': count_views,
-        'primary': primary,
-        'flickr_link': flickr_link,
-        'kk6gpv_link': kk6gpv_link,
-        'photos': photos
-        }
+gals = flickr.get_gals()
 
 
 @app.route('/')
@@ -112,9 +70,9 @@ def galleries():
         if idx < 6:
             frames.append(
                 {'caption': gals[gal]['title'] + ' - ' + str(gals[gal]['count_photos']),
-                'thumb': gals[gal]['primary'],
-                'kk6gpv_link': gals[gal]['kk6gpv_link']},
-                )
+                 'thumb': gals[gal]['primary'],
+                 'kk6gpv_link': gals[gal]['kk6gpv_link']},
+            )
             idx += 1
         else:
             rows.append(frames)
@@ -132,8 +90,8 @@ def gallery(id):
         if idx < 6:
             frames.append(
                 {'thumb': gals[id]['photos'][ph]['thumb'],
-                'kk6gpv_link': '/galleries/'+id+'/'+ph},
-                )
+                 'kk6gpv_link': '/galleries/'+id+'/'+ph},
+            )
             idx += 1
         else:
             rows.append(frames)
@@ -147,7 +105,7 @@ def image(id, ph):
     image = {
         'thumb': gals[id]['photos'][ph]['thumb'],
         'thumb': gals[id]['photos'][ph]['large'],
-        }
+    }
     return render_template('image.html', image=image, title='photo')
 
 
--- helpers/flickr.py
+++ helpers/flickr.py
@@ -1,30 +1,41 @@
-from __future__ import print_function
+#from __future__ import print_function
 
 import flickr_api as f
 
-f.set_keys(api_key='77a2ae7ea816558f00e4dd32249be54e',
-           api_secret='2267640a7461db21')
-#f.set_auth_handler('helpers/auth')
-f.set_auth_handler('auth')
 
-username = '- Adam Reeder -'
-u = f.Person.findByUserName(username)
-ps = u.getPhotosets()
+def get_gals():
+    f.set_keys(api_key='77a2ae7ea816558f00e4dd32249be54e',
+               api_secret='2267640a7461db21')
+    f.set_auth_handler('helpers/auth')
+    username = '- Adam Reeder -'
+    u = f.Person.findByUserName(username)
+    ps = u.getPhotosets()
 
-rows = []
-gals = []
-idx = 0
-for p in ps:
-    if idx < 4:
-        title = p.title
+    gals = {}
+    for p in ps:
         pid = p.id
-        photos = p.count_photos
-        views = p.count_views
-        primary = 'https://live.staticflickr.com/8203/'+p.primary+'_'+p.secret+'_q_d.jpg'
-        link = 'https://www.flickr.com/photos/adamreeder/albums/'+p.id
-        gals.append({'title':title, 'id':pid, 'photos':photos, 'views':views, 'primary':primary, 'link':link})
-        idx += 1
-    else:
-        rows.append(gals)
-        gals = []
-        idx = 0
+        title = p.title
+        count_photos = p.count_photos
+        count_views = p.count_views
+        primary = 'https://live.staticflickr.com/' + \
+            p.secret+'/'+p.primary+'_'+p.secret+'_q_d.jpg'
+        flickr_link = 'https://www.flickr.com/photos/adamreeder/albums/'+p.id
+        kk6gpv_link = '/galleries/'+p.id
+        photos = {}
+        phs = p.getPhotos()
+        for ph in phs:
+            photos[ph.id] = {
+                'thumb': 'https://live.staticflickr.com/'+ph.secret+'/'+ph.id+'_'+ph.secret+'_q_d.jpg',
+                'large': 'https://live.staticflickr.com/'+ph.secret+'/'+ph.id+'_'+ph.secret+'_h.jpg'
+            }
+        gals[pid] = {
+            'id': pid,
+            'title': title,
+            'count_photos': count_photos,
+            'count_views': count_views,
+            'primary': primary,
+            'flickr_link': flickr_link,
+            'kk6gpv_link': kk6gpv_link,
+            'photos': photos
+        }
+    return gals
