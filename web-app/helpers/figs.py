e1d1213ef1386ed13dec435a94060ac3b2da4b3b
areed145 <areed145@gmail.com>
2019-09-18 15:20:00 -0500

    Update figs.py

--- web-app/helpers/figs.py
+++ web-app/helpers/figs.py
@@ -490,8 +490,8 @@ def create_map_aprs(script, prop, time):
         r['latitude'] = np.round(row['latitude'], 3)
         r['longitude'] = np.round(row['longitude'], 3)
         r['speed'] = np.round(row['speed'], 2)
-        r['altitude'] = np.round(row['altitude'], 2)
-        r['course'] = np.round(row['course'], 0)
+        #r['altitude'] = np.round(row['altitude'], 2)
+        #r['course'] = np.round(row['course'], 0)
         rows.append(r)
     print(rows)
 
