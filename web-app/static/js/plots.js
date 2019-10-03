5c1291d436d206e92e3692e72131cd571e575802
areed145 <areed145@gmail.com>
2019-10-02 23:20:35 -0500

    update freq

--- web-app/static/js/plots.js
+++ web-app/static/js/plots.js
@@ -101,6 +101,15 @@ if ($('#iot').length > 0) {
 }
 
 if ($('#aprs').length > 0) {
+    var time_aprs = document.getElementById('time_aprs').value;
+    var time_int = 15;
+    if (time_aprs == 'm_5') {time_int = 1;}
+    if (time_aprs == 'h_1') {time_int = 2;}
+    if (time_aprs == 'h_6') {time_int = 10;}
+    if (time_aprs == 'd_1') {time_int = 20;}
+    if (time_aprs == 'd_2') {time_int = 30;}
+    if (time_aprs == 'd_7') {time_int = 60;}
+    if (time_aprs == 'd_30') {time_int = 60;}
 
     function Proc(rows) {
         var content = '';
@@ -211,7 +220,7 @@ if ($('#aprs').length > 0) {
                 // Plotly.plot('map_aprs', data);
                 // Plotly.deleteTraces('map_aprs', 0);    
             })
-    }, 1000 * 15);
+    }, 1000 * time_int);
 
 }
 
